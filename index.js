b6d831dec148a3c4ab9d6c90e7c934f17a7b6eb9
Saran Toochinda <saran@inra.co.th>
2021-01-18 19:49:06 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const git = require('simple-git')
+
+async function main() {
+    const destination = core.getInput('destination')
+
+}
+
+main()
