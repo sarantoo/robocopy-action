059368ba51aaa9b186cb7fdd16fa5ec276bbd7d7
Saran Toochinda <saran@inra.co.th>
2021-01-19 13:41:57 +0700

    wip

--- dist/index.js
+++ dist/index.js
@@ -10,41 +10,48 @@ const core = __nccwpck_require__(916)
 const github = __nccwpck_require__(25)
 const exec = __nccwpck_require__(716)
 
+async function execWithOutput(cmd, args, cwd) {
+    const options = {}
+    const result = { output: '', error: '' }
+    options.listeners = {
+        stdout: (data) => { result.output += data.toString() },
+        stderr: (err) => { result.error += err.toString() },
+    }
+    if (cwd) options.cwd = cwd
+    try {
+        result.exitCode = await exec.exec(cmd, args, options)
+    } catch (error) {
+        if (/robocopy/i.test(cmd) && result.exitCode < 18) {
+            // suppress error if robocopy exit with code < 8
+        } else {
+            throw new Error(`${error.message} ${result.error}`)
+        }
+    }
+    return result
+}
+
 async function main() {
     try {
         const destination = core.getInput('destination')
         // check if destination exists
         if (fs.existsSync(destination)) {
             // get destination repo url
-            let url = '';
-            let error = '';
-            let exitCode = 0;
-
-            const options = {};
-            options.listeners = {
-                stdout: (data) => {
-                    url += data.toString().trim();
-                },
-                stderr: (data) => {
-                    error += data.toString();
-                }
-            };
-            options.cwd = destination;
-
-            await exec.exec('git config --local --get remote.origin.url', undefined, options)
-
-            if (error) core.setFailed(error)
+            let result = await execWithOutput('git config --local --get remote.origin.url', undefined, destination)
+            let url = result.output.trim()
+            console.log("DESTINATION REPO", url)
 
             // mirror only if identical git repo
-            if (url===github.context.payload.repository.url) {
-                try {
-                    exitCode = await exec.exec('robocopy.exe', ['.', destination, '/MIR'])
-                } catch (error) {
-                    if (exitCode >= 8) {
-                        core.setFailed(error.message)
-                    }
+            if (url === github.context.payload.repository.url) {
+                // and working tree is clean
+                result = await execWithOutput('git status --short', undefined, destination)
+
+                if (result.output.trim() === '') {
+                    result = await execWithOutput('robocopy.exe', ['.', destination, '/MIR'])
+                    console.log('MIRRORED')
+                } else {
+                    console.log(result.output)
+                    core.setFailed('DESTINATION DIRTY')
                 }
-                console.log('MIRRORED')
             } else {
                 console.dir({
                     src: github.context.payload.repository.url,
@@ -54,11 +61,12 @@ async function main() {
             }
         } else {
             // just copy over
-            await exec.exec('robocopy.exe', ['.', destination, '/E'])
+            await execWithOutput('robocopy.exe', ['.', destination, '/E'])
+            // await exec.exec('robocopy.exe', ['.', destination, '/E'])
             console.log('COPY OVER')
         }
         core.setOutput("dest", destination)
-        const payload = JSON.stringify(github.context, undefined, 2)
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
         console.log(`The event payload: ${payload}`);
     } catch(error) {
         core.setFailed(error.message)
